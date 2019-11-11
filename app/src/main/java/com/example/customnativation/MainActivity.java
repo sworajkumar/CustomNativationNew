79877dc602e60a95ba69b3b287c59d8cc5f6fbf1
Admin <sworajkumardas@gmail.com>
2019-11-11 12:49:05 +0530

    Extra added something

--- app/src/main/java/com/example/customnativation/MainActivity.java
+++ app/src/main/java/com/example/customnativation/MainActivity.java
@@ -1,35 +1,148 @@
 package com.example.customnativation;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import android.os.Bundle;
-import android.view.MenuItem;
+import com.example.customnativation.fragment.AccountFragment;
+import com.example.customnativation.fragment.ActivityFragment;
+import com.example.customnativation.fragment.ArticleFragment;
+import com.example.customnativation.fragment.DashBoardFragment;
+import com.example.customnativation.fragment.EventFragment;
+import com.example.customnativation.fragment.SearchFragment;
+import com.example.customnativation.fragment.SettingFragment;
+import com.example.customnativation.fragment.forumFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
 
     private DrawerLayout mdrawerLayout;
     private ActionBarDrawerToggle mtoggle;
+    private BottomNavigationView bottomNavigationView;
+    private NavigationView navigationview;
+    private ImageView headerimage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mdrawerLayout=(DrawerLayout)findViewById(R.id.drawer);
-        mtoggle=new ActionBarDrawerToggle(this,mdrawerLayout,R.string.open,R.string.close);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNav);//Botom Navigation View
+        bottomNavigationView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        mdrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mtoggle = new ActionBarDrawerToggle(this, mdrawerLayout, R.string.open, R.string.close);
         mdrawerLayout.addDrawerListener(mtoggle);
         mtoggle.syncState();
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        navigationview=(NavigationView)findViewById(R.id.navigationview);
+        navigationview.setNavigationItemSelectedListener(this);
+
+        View headerview=navigationview.getHeaderView(0);
+        headerimage=headerview.findViewById(R.id.headerimage);
+        // LoadImage();
+        TextView name = (TextView) headerview.findViewById(R.id.name);
+        TextView mail = (TextView) headerview.findViewById(R.id.mailid);
+
+
+        if (savedInstanceState==null){
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new DashBoardFragment()).commit();
+            navigationview.setCheckedItem(R.id.dashboard);
+        }
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.bottom_nav_home:
+                                getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new DashBoardFragment()).commit();
+                                return true;
+                        case R.id.bottom_nav_actical:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new ArticleFragment()).commit();
+                            return true;
+                        case R.id.bottom_nav_forum:
+                            getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new forumFragment()).commit();
+                            return true;
+                        case R.id.bottom_nav_account://Bottom Navigation bottom right icon clicked
+                            getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new AccountFragment()).commit();
+                            return true;
+                    }
+                    return false;
+                }
+            };
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if (mtoggle.onOptionsItemSelected(item)) {
-        return true;
+            return true;
         }
         return super.onOptionsItemSelected(item);
+}
+
+   /* @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.navigation, menu);
+        return true;
+    }*/
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.dashboard) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new DashBoardFragment()).commit();
+
+        } else if (id == R.id.event) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new EventFragment()).commit();
+
+        } else if (id == R.id.search) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new SearchFragment()).commit();
+
+        } else if (id == R.id.setting) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new SettingFragment()).commit();
+
+        } else if (id == R.id.activities) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.nav_content_Frame, new ActivityFragment()).commit();
+
+        } else if (id == R.id.logout) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setMessage("Are you sure you want to Logout?")
+                    .setCancelable(false)
+                    .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            dialog.cancel();
+                        }
+                    });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+
+        mdrawerLayout = (DrawerLayout) findViewById(R.id.drawer);
+        mdrawerLayout.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/customnativation/fragment/EventFragment.java
@@ -0,0 +1,31 @@
+package com.example.customnativation.fragment;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.customnativation.R;
+
+/**
+ * A simple {@link EventFragment} subclass.
+ */
+public class EventFragment extends Fragment {
+
+
+    public EventFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_, container, false);
+    }
+
+}
